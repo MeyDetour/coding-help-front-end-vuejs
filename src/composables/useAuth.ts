2f59eba78cfe98eb715a21fb4fd810600923cf39
mey <meydetour@gmail.com>
2025-04-01 19:07:48 +0200

    feat: change handle error

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -9,26 +9,40 @@ export default function useAuth() {
 
   async function login(userData: { email: string; password: string }) {
     console.log("let's login ")
-    let response = await api('login', userData, 'POST')
-    response = await response.json()
-    if (response) {
-      localStorage.setItem('token', response.access)
-      await router.push('/private/dashboard')
+    try {
+      let response = await api('login', userData, 'POST')
+      response = await response.json()
+      console.log(response)
+      if (response.access) {
+        console.log('get access')
+        localStorage.setItem('token', response.access)
+        await router.push('/private/dashboard')
+        return {}
+      }
+    } catch (e) {
+      return { error: e.message }
     }
   }
 
-  async function register(userData: { email: string; password: string , username : string }) {
+  async function register(userData: { email: string; password: string; username: string }) {
     console.log("let's register ")
-    let response = await api('register', userData, 'POST')
-    response = await response.json()
-    if (response) {
-      localStorage.setItem('token', response.access)
-      await router.push('/private/dashboard')
+    try {
+      let response = await api('register', userData, 'POST')
+      response = await response.json()
+      console.log(response)
+      if (response.message === "ok") {
+        await router.push('/login')
+        return {}
+      }
+    } catch (e) {
+      return { error: e.message }
     }
   }
+
   async function logout() {
-   localStorage.removeItem("token")
+    localStorage.removeItem('token')
+    await router.push('/')
   }
 
-  return { login,logout,register }
+  return { login, logout, register }
 }
