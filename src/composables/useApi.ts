a2ceaca082cce3467c75cf992f287f6a89a3d914
mey <meydetour@gmail.com>
2025-04-01 19:07:44 +0200

    feat: change handle error

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,43 +1,45 @@
 import { useRouter } from 'vue-router'
-import { toast } from 'vue3-toastify'
-
 
 export default function useApi() {
+  const router = useRouter()
 
-  const router = useRouter();
-  async function api(url:string, bodyContent:object|null, method:'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' = 'GET') {
+  async function api(
+    url: string,
+    bodyContent: object | null,
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' = 'GET',
+    needToken: boolean = true,
+  ) {
     try {
-      const baseUrl = "https://codinghelp.md-genos.com/"
-      console.log(`lets use api for : ${baseUrl}${url} with methode ${method} and body ${bodyContent}`)
-      const token = localStorage.getItem('token');
+      const baseUrl = 'https://codinghelp-api.meydetour.com/'
+      console.log(
+        `lets use api for : ${baseUrl}${url} with methode ${method} and body ${bodyContent}`,
+      )
+      const token = localStorage.getItem('token')
       const response = await fetch(`${baseUrl}${url}`, {
         method: method,
         headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
+          Authorization: `Bearer ${token}`,
+          ...(needToken ? { 'Content-Type': 'application/json' } : {}),
         },
-       ...( bodyContent ? {  body: JSON.stringify(bodyContent)  } : {})
+        ...(bodyContent ? { body: JSON.stringify(bodyContent) } : {}),
       })
 
       if (response.status === 401) {
-        toast.error("Error while authentification, please login")
         router.push('/login')
       }
       if (!response.ok) {
-        console.log("response pas ok ")
-        const errorData = await response.json().catch(() => ({}));
-        throw Object.assign(new Error(errorData.message || response.statusText), { status: response.status });
-
+        console.log('response pas ok ')
+        const errorData = await response.json().catch(() => ({}))
+        throw Object.assign(new Error(errorData.message || response.statusText), {
+          status: response.status,
+        })
       }
 
-      return   response
-
+      return response
     } catch (error) {
-      toast.error("Oups, there is an error : "+ error);
-
-      console.error('Login error:', error);
-      return Promise.reject(error);
+      return Promise.reject(error)
     }
   }
-  return {api}
+
+  return { api }
 }
